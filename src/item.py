3bb3cdae136bfa8400ca763981771a906a6e94be
nadine-tim <nadinetimeiko@gmail.com>
2023-04-09 00:39:46 +0300

    homework-2 done

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+import math
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,9 +16,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+        self.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            print("Exception: длина наименования товара больше 10 симвовов")
+            # raise ValueError("длина наименования товара больше 10 симвовов")
+        self.__name = value
 
     def calculate_total_price(self) -> float:
         """
@@ -30,3 +45,15 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @staticmethod
+    def instantiate_from_csv():
+        Item.all = []
+        with open("../src/items.csv", 'r', encoding='WINDOWS-1251') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",")
+            for dct in reader:
+                item = Item(dct['name'], dct['price'], dct['quantity'])
+
+    @staticmethod
+    def string_to_number(number: str):
+        return math.floor(float(number))
--- tests/test_item.py
+++ tests/test_item.py
@@ -22,22 +22,3 @@ class TestItem:
         item.apply_discount()
         assert item.price == 5.0
         item.pay_rate = 1.0
-
-    # def test_apply_discount_with_zero_pay_rate(self, item):
-    #     item.pay_rate = 0.0
-    #     item.apply_discount()
-    #     assert item.price == 0.0
-    #     item.pay_rate = 1.0
-
-    # def test_apply_discount_with_negative_pay_rate(self, item):
-    #     item.pay_rate = -0.5
-    #     with pytest.raises(ValueError):
-    #         item.apply_discount()
-    #     item.pay_rate = 1.0
-
-    # def test_item_instances_added_to_all_list(self, item):
-    #     assert item in Item.all
-    #     assert Item.all.count(item) == 1
-    #
-    # def test_str_representation(self, item):
-    #     assert str(item) == "Test Item - $10.0 x 2"
