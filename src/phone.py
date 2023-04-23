c9987490c0871d700c8e8e0324b684a8ece48d11
nadine-tim <nadinetimeiko@gmail.com>
2023-04-23 22:44:04 +0300

    fix bugs hw-4

--- src/phone.py
+++ src/phone.py
@@ -8,9 +8,9 @@ class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
         if number_of_sim <= 0:
             raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self.number_of_sim = number_of_sim
 
 
     def __repr__(self):
